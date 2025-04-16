f790c1a99d29006db54038892393ead43e0f0512
mostafa216 <athadalarab123@gmail.com>
2025-04-16 21:34:29 +0200

    Update dependencies and refactor plugin registrations
    - Updated versions of media_kit_libs_android_video (1.3.7), media_kit_libs_linux (1.2.1), media_kit_libs_windows_video (1.0.11), media_kit_video (1.3.0), volume_controller (3.3.3), wakelock_plus (1.2.11), and screen_brightness_android (2.1.1) in .flutter-plugins and .flutter-plugins-dependencies files.
    - Removed screen_brightness and media_kit_native_event_loop from generated plugin lists in CMake files for both Linux and Windows platforms.
    - Updated plugin registration in macOS to replace screen_brightness_macos with volume_controller.
    - Updated pubspec.lock to reflect new versions of various dependencies, including media_kit (1.2.0) and screen_brightness (2.0.1).

--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -5,11 +5,10 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   media_kit_libs_windows_video
   media_kit_video
-  screen_brightness_windows
+  volume_controller
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-  media_kit_native_event_loop
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
